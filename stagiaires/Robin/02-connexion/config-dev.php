bd05bda3e13b8f192b8cf239ad72876bc58835a8
KukichaBXL <craeyerobin@gmail.com>
2026-04-24 12:17:00 +0200

    02-connexion

--- stagiaires/Robin/02-connexion/config-dev.php
+++ stagiaires/Robin/02-connexion/config-dev.php
@@ -1,5 +1,5 @@
 <?php
-# config-dev.php
+# stagiaires/Robin/02-connexion/config-dev.php
 # fichier de configuration de PDO en mode développement.
 # Ce fichier peut être sur github car il ne contient pas de données sensibles
 const DB_CONNECT_TYPE = "mysql"; // MySQL et MariaDB
@@ -9,3 +9,4 @@ const DB_CONNECT_NAME = "listepays"; // Nom de la DB
 const DB_CONNECT_CHARSET = "utf8mb4"; // Encodage de notre connexion
 const DB_CONNECT_USER = "root"; // Login par défaut
 const DB_CONNECT_PWD = ""; // Pas de mot de passe par défaut
+
